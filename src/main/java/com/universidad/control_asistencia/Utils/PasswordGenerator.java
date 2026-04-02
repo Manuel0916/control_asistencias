34662a0df630bf509453a77dbad6b1786736f8bd
Víctor José Castillo Castro <castillocastrovictorjose@hotmail.com>
2026-04-01 22:42:20 -0500

    Actualizar todo el proyecto

--- src/main/java/com/universidad/control_asistencia/Utils/PasswordGenerator.java
+++ src/main/java/com/universidad/control_asistencia/Utils/PasswordGenerator.java
@@ -3,13 +3,17 @@ package com.universidad.control_asistencia.Utils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordGenerator {
+
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
         String nuevaContraCoordinador = encoder.encode("123456");
         String nuevaContraEstudiante = encoder.encode("123456");
+        String nuevaContraDocente = encoder.encode("123456");
 
         System.out.println("Coordinador: " + nuevaContraCoordinador);
         System.out.println("Estudiante: " + nuevaContraEstudiante);
+        System.out.println("Docente: " + nuevaContraDocente);
     }
+
 }
--- src/main/java/com/universidad/control_asistencia/service/interfaces/LoginConfigInterface.java
+++
@@ -1,9 +0,0 @@
-package com.universidad.control_asistencia.service.interfaces;
-
-import com.universidad.control_asistencia.model.LoginConfig;
-
-public interface LoginConfigInterface {
-    boolean isLoginHabilitado(String rol);
-    LoginConfig cambiarEstadoLogin(String rol);
-    LoginConfig obtenerEstado(String rol);
-}
